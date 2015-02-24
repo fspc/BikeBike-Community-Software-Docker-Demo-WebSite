6fe27f4873e3c7a830a030274233c949e9a5221e
Jonathan Rosenbaum <gnuser@gmail.com>
2015-02-24 06:37:36 +0000

    Add a lot in "Show More".
    1).  Show More button shows more information that is mostly of interest
    to developers.

---
+++ js/bikebike.js
@@ -0,0 +1,31 @@
+$(function(){
+
+	"use strict";
+
+	// extra information about each demo
+	var demos = ["ybdb", "bikebinder", "bikeshed", "freehub", "bikekitchenpos"];
+	
+	$.each(demos, function(key, value) { 
+		show_more($('#' + value), $('#' + value + '_button'));
+	} );
+
+	function show_more(demo,demo_button) {
+	  	$(demo).hide();
+	  	var c=0;
+	  	$(demo_button).click(function(e){
+	  		e.preventDefault();
+		  	if (c == 0) {
+			$(demo).slideDown();
+				$(this).attr("value","Show Less");
+				c++;
+		  	} else {
+		   	$(demo).slideUp();
+		   	$(this).attr("value","Show More");
+		    	c--;
+		    }
+	  	 } );  
+  	 }
+  	 
+ });
+  	 
+  	 
