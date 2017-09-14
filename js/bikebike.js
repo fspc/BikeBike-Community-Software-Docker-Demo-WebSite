64715b04bc771679e78903ef7a4dac16c477531c
Jonathan Rosenbaum <gnuser@gmail.com>
2017-09-14 09:48:14 +0000

    Updated to bikelover.org and new demos!

--- js/bikebike.js
+++ js/bikebike.js
@@ -3,7 +3,7 @@ $(function(){
 	"use strict";
 
 	// extra information about each demo
-	var demos = ["ybdb", "bikebinder", "bikeshed", "freehub", "bikekitchenpos", "signinapp"];
+	var demos = ["ybdb", "bikebinder", "bikeshed", "freehub", "biketree", "bikekitchenpos", "signinapp", "vtracklery", "vtracklery2", "bike-database", "workstand"];
 	
 	$.each(demos, function(key, value) { 
 		show_more($('#' + value), $('#' + value + '_button'));
